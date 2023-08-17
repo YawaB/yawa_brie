9813f04420f03b69447143a9be06655ce9c9f92b
YawaB <mawuviwobrinda@gmail.com>
2023-08-17 05:26:11 +0100

    favicon and scroll-down for mobile

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,25 +1,32 @@
-import React from 'react'
-import './header.css'
-import CTA from './CTA'
-import ME from '../../assets/images/me.png'
-import HeaderSocial from './HeaderSocials'
+import React from "react";
+import "./header.css";
+import CTA from "./CTA";
+import { BsArrowDownCircle } from "react-icons/bs";
+import ME from "../../assets/images/me.png";
+import HeaderSocial from "./HeaderSocials";
 const Header = () => {
   return (
     <header>
-    <div className="container header__container">
-      <h5>Hello I'am</h5>
-      <h1>Yawa Brinda</h1>
-      <h5 className="text-light">Fullstack Developper</h5>
-      <CTA />
-      <HeaderSocial />
-      <div >
-        <img src={ME} alt="me"className="me"/>
+      <div className="container header__container">
+        <h5>Hello I'am</h5>
+        <h1>Yawa Brinda</h1>
+        <h5 className="text-light">Fullstack Developper</h5>
+        <CTA />
+        <HeaderSocial />
+        <div>
+          <img src={ME} alt="me" className="me" />
+        </div>
+        <a href="#contact" className="scroll__down">
+          Scroll down
+        </a>
+        <a href="#contact" className="scroll__down-mobile">
+          <BsArrowDownCircle />
+        </a>
       </div>
-      <a href="#contact" className='scroll__down'>Scroll down</a>
-      
-    </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+
+
+export default Header;
